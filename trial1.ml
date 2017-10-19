f28b0db72d6e7786c9b85973fa8c46ff8698808d
Abhinav Sharma <abhi18av@gmail.com>
2017-10-19 10:03:35 +0530

    working on the code walkthroughs in RWO

--- trial1.ml
+++ trial1.ml
@@ -1,10 +1,23 @@
-open Core_kernel;;
+
+let () =
+    print_endline (string_of_int ( 3 + 4 ));
+    print_endline (string_of_int ( 8 / 2 ));
+    print_endline (string_of_float ( 3.5 +. 6. ));
+    print_endline (string_of_int ( 30_000_000 / 300_000 ));
+    print_endline (string_of_float (sqrt 9.))
 
 
-let ratio x y = Float.of_int x /. Float.of_int y;;
 
 
-print_float ratio 4 7 ;;
+
+
+(* open Core_kernel;;
+ * 
+ * 
+ * let ratio x y = Float.of_int x /. Float.of_int y;;
+ * 
+ * 
+ * print_float ratio 4 7 ;; *)
 
 
 
